eb190d15c162124b38774859fa66d5bd5502e8c5
stefan <stefan_paksa@icloud.com>
2023-04-10 00:24:43 +0300

    add alt support

--- pkg/vulnsrc/vulnsrc.go
+++ pkg/vulnsrc/vulnsrc.go
@@ -2,27 +2,17 @@ package vulnsrc
 
 import (
 	"github.com/aquasecurity/trivy-db/pkg/types"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/alma"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/alpine"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/amazon"
-	archlinux "github.com/aquasecurity/trivy-db/pkg/vulnsrc/arch-linux"
+	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/alt"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/bundler"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/composer"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/debian"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/ghsa"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/glad"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/govulndb"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/mariner"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/node"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/nvd"
-	oracleoval "github.com/aquasecurity/trivy-db/pkg/vulnsrc/oracle-oval"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/osv"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/photon"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/redhat"
 	redhatoval "github.com/aquasecurity/trivy-db/pkg/vulnsrc/redhat-oval"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/rocky"
-	susecvrf "github.com/aquasecurity/trivy-db/pkg/vulnsrc/suse-cvrf"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/ubuntu"
 	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/wolfi"
 )
 
@@ -35,21 +25,21 @@ var (
 	// All holds all data sources
 	All = []VulnSrc{
 		// NVD
-		nvd.NewVulnSrc(),
-
+		//nvd.NewVulnSrc(),
 		// OS packages
-		alma.NewVulnSrc(),
-		alpine.NewVulnSrc(),
-		archlinux.NewVulnSrc(),
-		redhat.NewVulnSrc(),
+		alt.NewVulnSrc(),
+		//alma.NewVulnSrc(),
+		//alpine.NewVulnSrc(),
+		//archlinux.NewVulnSrc(),
+		//redhat.NewVulnSrc(),
 		redhatoval.NewVulnSrc(),
-		debian.NewVulnSrc(),
-		ubuntu.NewVulnSrc(),
-		amazon.NewVulnSrc(),
-		oracleoval.NewVulnSrc(),
-		rocky.NewVulnSrc(),
-		susecvrf.NewVulnSrc(susecvrf.SUSEEnterpriseLinux),
-		susecvrf.NewVulnSrc(susecvrf.OpenSUSE),
+		//debian.NewVulnSrc(),
+		//ubuntu.NewVulnSrc(),
+		//amazon.NewVulnSrc(),
+		//oracleoval.NewVulnSrc(),
+		//rocky.NewVulnSrc(),
+		//susecvrf.NewVulnSrc(susecvrf.SUSEEnterpriseLinux),
+		//susecvrf.NewVulnSrc(susecvrf.OpenSUSE),
 		photon.NewVulnSrc(),
 		mariner.NewVulnSrc(),
 		wolfi.NewVulnSrc(),
